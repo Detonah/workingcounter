e4990db9a2216f75a62fe7e52270bae40be1dcba
Detonah <st.adetona@gmail.com>
2022-10-26 15:03:50 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+
+// let body = document.body.style.backgroundColor= "brown"
+
+// let btn = document.querySelector('.inc')
+
+// function colored(){
+// let body = document.body
+// body.style.backgroundColor = 'brown'
+
+// }
+
+// btn.addEventListener('click', colored)
+
+let heading =document.querySelector('h1');
+let inc =document.querySelector(".inc");
+let res =document.querySelector(".res");
+let dec =document.querySelector(".dec");
+
+function DECREASE (){
+heading.innerText--
+}
+dec.addEventListener('click', DECREASE)
+
+
+function RESET (){
+    heading.innerText =('0')
+}
+res.addEventListener('click', RESET)
+
+
+function INCREASE (){
+    heading.innerText++
+    }
+
+inc.addEventListener('click', INCREASE)
+   
+
+
+// let para = document.querySelector('h1')
+// console.log(para);
+// para.innerText = 'Detonah'
+
+
+// const btn1 = document.querySelector(".res");
+// const header1 = document.querySelector ("h1")
+
+// function changeText(){
+// let newCounter = document.querySelector("h1");
+// newCounter.innerText = "1"
+// }
+
+// btn1.addEventListener("click", changeText)
